6e91417cb11b1182c63db81960ab1810e122ac3d
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2017-06-25 11:28:40 +1000

    Code

--- app/src/main/java/com/evgenii/sixbouncingbuttons/MainActivity.java
+++ app/src/main/java/com/evgenii/sixbouncingbuttons/MainActivity.java
@@ -2,6 +2,10 @@ package com.evgenii.sixbouncingbuttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void didTapButton(View view) {
+        Button button = (Button)findViewById(R.id.button);
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        button.startAnimation(myAnim);
     }
 }
